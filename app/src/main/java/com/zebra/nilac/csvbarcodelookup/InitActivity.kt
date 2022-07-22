781f01beba9a197f5ff754ee21f02615fff1db27
Calin Neamtu <nilac8991@gmail.com>
2022-07-22 14:53:03 +0200

    Pre-grant manage external storage permission through MX
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/nilac/csvbarcodelookup/InitActivity.kt
+++ app/src/main/java/com/zebra/nilac/csvbarcodelookup/InitActivity.kt
@@ -1,12 +1,15 @@
 package com.zebra.nilac.csvbarcodelookup
 
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.zebra.nilac.csvbarcodelookup.databinding.ActivityInitBinding
 import com.zebra.nilac.csvbarcodelookup.databinding.ActivityMainBinding
 import com.zebra.nilac.emdkloader.EMDKLoader
+import com.zebra.nilac.emdkloader.ProfileLoader
 import com.zebra.nilac.emdkloader.interfaces.EMDKManagerInitCallBack
+import com.zebra.nilac.emdkloader.interfaces.ProfileLoaderResultCallback
 
 class InitActivity : AppCompatActivity() {
 
@@ -21,7 +24,7 @@ class InitActivity : AppCompatActivity() {
         initEMDKManager()
     }
 
-    fun initEMDKManager() {
+    private fun initEMDKManager() {
         //Initialising EMDK First...
         Log.i(TAG, "Initialising EMDK Manager")
         mBinder.loadingStatusText.text = getString(R.string.init_screen_emdk_check)
@@ -36,9 +39,36 @@ class InitActivity : AppCompatActivity() {
                 Log.i(TAG, "EMDK Manager was successfully initialised")
                 mBinder.loadingStatusText.text = getString(R.string.init_screen_emdk_success)
 
-                //TODO
+                grantManageExternalStoragePermission()
             }
         })
+    }
+
+    private fun grantManageExternalStoragePermission() {
+        Log.i(TAG, "Granting External Storage Permission through MX")
+        ProfileLoader().processProfile(
+            "AccessManager",
+            null,
+            object : ProfileLoaderResultCallback {
+                override fun onProfileLoadFailed(message: String) {
+                    runOnUiThread {
+                        mBinder.loadingStatusText.text =
+                            getString(R.string.init_process_storage_permission_profile_failed)
+                    }
+                }
+
+                override fun onProfileLoaded() {
+                    runOnUiThread {
+                        if (Environment.isExternalStorageManager()) {
+                            mBinder.loadingStatusText.text =
+                                getString(R.string.init_process_storage_permission_profile_success)
+                        } else {
+                            mBinder.loadingStatusText.text =
+                                getString(R.string.init_process_storage_permission_profile_failed)
+                        }
+                    }
+                }
+            })
     }
 
     companion object {
